65f53e373c14a3767b03b05629ecfd16a940f702
Joshua Bennett <foxandwaffle@gmail.com>
2017-08-22 18:50:21 -0700

    initial commit

--- packr.py
+++ packr.py
@@ -1,14 +1,1 @@
-# The dictionary will look something like:
-# {'Andrew Chalkley': ['jQuery Basics', 'Node.js Basics'],
-#  'Kenneth Love': ['Python Basics', 'Python Collections']}
-#
-# Each key will be a Teacher and the value will be a list of courses.
-#
-# Your code goes below here.
-
-
-def num_teachers(argument):
-	argument = {}
-	
-
-
+print("Hello world!")
